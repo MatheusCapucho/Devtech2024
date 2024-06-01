6d13e0bd1e6db6140cc5f3c5077627ca845f84d1
MatheusCapucho <matheusfcapucho@gmail.com>
2024-06-01 01:28:05 -0300

    Interaction System Initial Commit

---
+++ Devtech/Assets/_CScripts/InteractionSystem/PlayerInteraction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
