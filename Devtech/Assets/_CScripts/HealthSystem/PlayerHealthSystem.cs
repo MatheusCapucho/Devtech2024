952a90c6087b28cf7aec47e8384191bd35ac58d7
MatheusCapucho <matheusfcapucho@gmail.com>
2024-06-01 00:12:11 -0300

    InvulnerabilityFrames

--- Devtech/Assets/_CScripts/HealthSystem/PlayerHealthSystem.cs
+++ Devtech/Assets/_CScripts/HealthSystem/PlayerHealthSystem.cs
@@ -7,6 +7,10 @@ public class PlayerHealthSystem : MonoBehaviour, ITakeDamage
     [SerializeField]
     private int maxHealth = 3;
     private int currentHealth;
+    [SerializeField]
+    private float invulnerabilityTime = .5f;
+
+    private bool playerCanTakeDamage = true;
 
     void Awake()
     {
@@ -15,6 +19,10 @@ public class PlayerHealthSystem : MonoBehaviour, ITakeDamage
 
     public void TakeDamage(int amount)
     {
+        if (!playerCanTakeDamage)
+            return;
+
+        StartCoroutine(InvulnerabilityFrames());
         currentHealth -= amount;
         PlayerHealthSystemUI.OnHealthChanged?.Invoke(currentHealth);
         Debug.Log(currentHealth + " current ");
@@ -30,14 +38,18 @@ public class PlayerHealthSystem : MonoBehaviour, ITakeDamage
         
         PlayerHealthSystemUI.OnHealthChanged?.Invoke(currentHealth);
     }
-
-
     private void Die()
     {
         // die
         // startcoroutine: animation dying, fade out , UI, changescene
     }
 
+    IEnumerator InvulnerabilityFrames()
+    {
+        playerCanTakeDamage = false;
+        yield return new WaitForSeconds(invulnerabilityTime);
+        playerCanTakeDamage = true;
+    }
 
 
 }
